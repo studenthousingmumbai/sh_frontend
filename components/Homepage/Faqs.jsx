0cb4ddd7cb49e37e95585c486919e917fb9c33c0
Tanay Kulkarni <tanaykulkarni7@gmail.com>
2023-04-07 15:59:05 +0530

    updated faqs section on homepage

--- components/Homepage/Faqs.jsx
+++ components/Homepage/Faqs.jsx
@@ -46,8 +46,8 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <div className="bg-white h-[100vh] flex ">
-      <div className="m-auto mx-auto max-w-7xl py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-3xl divide-y-2 divide-gray-200">
           <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Frequently asked questions
