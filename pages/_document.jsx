0af562b6a138c7f42cc779a048f124da25fd5cd8
Tanay Kulkarni <tanaykulkarni7@gmail.com>
2023-05-19 15:41:56 +0530

    updated favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,10 +4,13 @@ export default function Document() {
   return (
     <Html className="h-full bg-white antialiased" lang="en">
       <Head>
+          <title>Student Housing</title>
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700;900&display=swap" rel="stylesheet"/>
+          <meta name="description" content="Student Housing in Mumbai" />
+          <link rel="icon" href="/sh_logo.png" />
       </Head>
       <body className='h-full'>
           <Main />
