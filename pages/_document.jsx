133d9ec1e1e8be79d6adb4cfea0119e33f4b4e2a
Tanay Kulkarni <tanaykulkarni7@gmail.com>
2023-06-07 16:16:30 +0530

    added gtags & clarity tag

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,6 +11,41 @@ export default function Document() {
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700;900&display=swap" rel="stylesheet"/>
           <meta name="description" content="Student Housing in Mumbai" />
           <link rel="icon" href="/sh_logo.png" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-DEFDC3GBPL"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-DEFDC3GBPL');
+              `,
+            }}
+          />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-75045456-1"></script>
+          <script
+             dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+    
+                gtag('config', 'UA-75045456-1');
+              `,
+            }}
+          />
+          <script type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "hf3i0kvqqu");
+              `,
+            }}
+          />
       </Head>
       <body className='h-full'>
           <Main />
