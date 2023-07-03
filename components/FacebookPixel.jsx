7a9f4898ec5c3b4d0e9a82ea56682e5ed383a749
Tanay Kulkarni <tanaykulkarni7@gmail.com>
2023-07-03 13:10:13 +0530

    updated facebook pixel code

--- components/FacebookPixel.jsx
+++ components/FacebookPixel.jsx
@@ -4,7 +4,6 @@ export default function FacebookPixel() {
   return (
     <>
       <Script
-        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
             !function(f,b,e,v,n,t,s)
