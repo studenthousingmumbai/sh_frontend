330fa0254152b656933203555d6aec0b87582a5b
Tanay Kulkarni <tanaykulkarni7@gmail.com>
2023-07-03 13:30:23 +0530

    updated facebook pixel code

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,24 +1,26 @@
 import '../styles/globals.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+
+function FacebookPixel() {
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('983766016309702');
+        ReactPixel.pageView();
+
+        Router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  });
+
+  return null;
+}
 
 function MyApp({ Component, pageProps }) {
   const google_client_id = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-  const router = useRouter(); 
-
-  useEffect(() => {
-    import('react-facebook-pixel')
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init('983766016309702')
-        ReactPixel.pageView()
-
-        router.events.on('routeChangeComplete', () => {
-          ReactPixel.pageView()
-        })
-      })
-  }, [router.events])
 
   useEffect(() => {
     window.addEventListener('phx:page-loading-stop', (event) => {
@@ -38,6 +40,7 @@ function MyApp({ Component, pageProps }) {
   
   return (
     <GoogleOAuthProvider clientId={google_client_id}>
+      <FacebookPixel/>
       <Component {...pageProps} />
     </GoogleOAuthProvider>
   )
