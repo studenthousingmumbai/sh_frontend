2840fd8e6c9c4d27e57b086d225cf3c89a35c327
Tanay Kulkarni <tanaykulkarni7@gmail.com>
2023-07-14 16:01:58 +0530

    added gtags to _document and thank-you pages

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -46,6 +46,18 @@ export default function Document() {
             `,
           }}
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-925883133"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-925883133');
+            `,
+          }}
+        />
 
         {/* <script type='text/javascript'
           dangerouslySetInnerHTML={{ 
