d0ca7153841b24f9bc21b53bb79a32c3dc59c66c
Tanay Kulkarni <tanaykulkarni7@gmail.com>
2023-09-05 23:29:28 +0530

    updated refer and earn text

--- pages/refer-and-earn.jsx
+++ pages/refer-and-earn.jsx
@@ -62,7 +62,7 @@ export default function ReferAndEarn() {
         <div className='flex flex-col lg:flex-row w-full p-6 bg-refer-and-earn-banner bg-cover bg-no-repeat gap-4 lg:gap-0'>
             <div className='w-full lg:w-1/2 flex flex-col items-center lg:items-end justify-center gap-4 lg:gap-8'>
                 <div className='w-[70%] text-[41px] leading-[52px] text-center lg:text-left font-semibold'>
-                    Refer your friends to Student Housing and get Vouchers upto 15,000/- INR.
+                  Refer your friends to Student Housing and get rewarded with flat ₹10,000/-
                 </div>
                 <div className='w-[70%] text-xl font-semibold text-center lg:text-left'>
                     You are just steps away from getting your reward
