26b648df30406a2e2ba8fe0b66ba86d7afd87e15
studenthousingmumbai <117896643+studenthousingmumbai@users.noreply.github.com>
2026-01-08 11:52:12 +0530

    Update [slug].jsx

--- pages/blogs/[slug].jsx
+++ pages/blogs/[slug].jsx
@@ -9,26 +9,65 @@ import Link from "next/link";
 export default function Blog({ blog }) {
   if (!blog) return null;
 
-  return (
-    <Layout>
-      <Head>
-        {/* ✅ PAGE TITLE */}
-        <title>{blog.pageTitle || blog.title}</title>
+  const pageTitle = blog.pageTitle || blog.title;
+  const description = blog.description || "";
 
-        {/* ✅ META TAGS */}
-        {blog.metaTags?.map((tag, index) =>
-          tag.metaName ? (
-            <meta key={index} name={tag.metaName} content={tag.metaContent} />
-          ) : tag.metaProperty ? (
-            <meta
-              key={index}
-              property={tag.metaProperty}
-              content={tag.metaContent}
-            />
-          ) : null
+  return (
+    <>
+      {/* ================= SEO HEAD (MUST BE OUTSIDE LAYOUT) ================= */}
+      <Head>
+        <title>{pageTitle}</title>
+
+        {description && (
+          <meta name="description" content={description} />
         )}
 
-        {/* ✅ SCHEMA MARKUP */}
+        {/* ===== Open Graph (optional but recommended) ===== */}
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={pageTitle} />
+        {description && (
+          <meta property="og:description" content={description} />
+        )}
+        {blog.coverPhoto?.url && (
+          <meta property="og:image" content={blog.coverPhoto.url} />
+        )}
+
+        {/* ===== Twitter Card ===== */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={pageTitle} />
+        {description && (
+          <meta name="twitter:description" content={description} />
+        )}
+        {blog.coverPhoto?.url && (
+          <meta name="twitter:image" content={blog.coverPhoto.url} />
+        )}
+
+        {/* ===== Extra meta from Hygraph (OG / Twitter only) ===== */}
+        {blog.metaTags?.map((tag, index) => {
+          if (tag.metaProperty) {
+            return (
+              <meta
+                key={`prop-${index}`}
+                property={tag.metaProperty}
+                content={tag.metaContent}
+              />
+            );
+          }
+
+          if (tag.metaName && tag.metaName !== "Title") {
+            return (
+              <meta
+                key={`name-${index}`}
+                name={tag.metaName}
+                content={tag.metaContent}
+              />
+            );
+          }
+
+          return null;
+        })}
+
+        {/* ===== Schema Markup ===== */}
         {blog.schemaMarkup && (
           <script
             type="application/ld+json"
@@ -39,37 +78,42 @@ export default function Blog({ blog }) {
         )}
       </Head>
 
-      <div className="w-[70%] md:w-[65%] lg:w-[55%] mx-auto py-16 lg:py-20">
-        {/* Breadcrumb */}
-        <div className="text-xs sm:text-sm md:text-base">
-          <span className="text-brandColor">
-            <Link href="/">Home&nbsp;&nbsp;</Link>/&nbsp;&nbsp;
-            <Link href="/blogs">Blogs&nbsp;&nbsp;</Link>/&nbsp;&nbsp;
-          </span>
-          <span>{blog.title}</span>
-        </div>
+      {/* ================= PAGE LAYOUT ================= */}
+      <Layout>
+        <div className="w-[70%] md:w-[65%] lg:w-[55%] mx-auto py-16 lg:py-20">
+          {/* Breadcrumb */}
+          <div className="text-xs sm:text-sm md:text-base">
+            <span className="text-brandColor">
+              <Link href="/">Home&nbsp;&nbsp;</Link>/&nbsp;&nbsp;
+              <Link href="/blogs">Blogs&nbsp;&nbsp;</Link>/&nbsp;&nbsp;
+            </span>
+            <span>{blog.title}</span>
+          </div>
 
-        {/* Cover Image */}
-        <div className="rounded-xl mt-5 md:mt-10 w-full">
-          <img
-            className="rounded-xl w-full"
-            src={blog.coverPhoto.url}
-            alt={blog.title}
+          {/* Cover Image */}
+          {blog.coverPhoto?.url && (
+            <div className="rounded-xl mt-5 md:mt-10 w-full">
+              <img
+                className="rounded-xl w-full"
+                src={blog.coverPhoto.url}
+                alt={blog.title}
+              />
+            </div>
+          )}
+
+          {/* Published Date */}
+          <div className="mt-3 md:mt-5 text-sm md:text-lg text-brandColor font-[500]">
+            {moment(blog.createdOn).format("ll")}
+          </div>
+
+          {/* Blog Content */}
+          <div
+            className={styles["blog-content"]}
+            dangerouslySetInnerHTML={{ __html: blog.content.html }}
           />
         </div>
-
-        {/* Date */}
-        <div className="mt-3 md:mt-5 text-sm md:text-lg text-brandColor font-[500]">
-          {moment(blog.createdOn).format("ll")}
-        </div>
-
-        {/* Content */}
-        <div
-          className={styles["blog-content"]}
-          dangerouslySetInnerHTML={{ __html: blog.content.html }}
-        />
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 }
 
@@ -77,7 +121,7 @@ export async function getServerSideProps({ params }) {
   try {
     const { data } = await client.query({
       query: gql`
-        query Blog($slug: String!) {
+        query BlogBySlug($slug: String!) {
           blogs(where: { slug: $slug }) {
             title
             pageTitle
@@ -109,7 +153,7 @@ export async function getServerSideProps({ params }) {
       },
     };
   } catch (error) {
-    console.error(error);
+    console.error("Blog fetch error:", error);
     return {
       props: {
         blog: null,
