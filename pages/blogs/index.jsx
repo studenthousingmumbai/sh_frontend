645a559ad01c01e9f6c33a4733da5d2b494e922f
studenthousingmumbai <117896643+studenthousingmumbai@users.noreply.github.com>
2026-01-08 10:59:21 +0530

    Update index.jsx

--- pages/blogs/index.jsx
+++ pages/blogs/index.jsx
@@ -78,6 +78,7 @@ export async function getServerSideProps() {
             id
             publishedAt
             title
+            pageTitle
             updatedAt
             slug
           }
