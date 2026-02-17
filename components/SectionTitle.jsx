a7de7743013a4aecf2845d64c57142fdb184a8ee
studenthousingmumbai <117896643+studenthousingmumbai@users.noreply.github.com>
2026-02-17 12:40:49 +0530

    Update SectionTitle.jsx

--- components/SectionTitle.jsx
+++ components/SectionTitle.jsx
@@ -1,10 +1,16 @@
-export default function SectionTitle({ title, className }) {
+export default function SectionTitle({ 
+  title, 
+  className, 
+  tag = "h2" 
+}) {
+  const Tag = tag;
+
   return (
     <div className={`${className} flex flex-col justify-center items-center`}>
-      <div className={` pageTitle flex flex-col`}>
-        <div className={`${className?.includes("text-white") && "text-white"}`}>
+      <div className="pageTitle flex flex-col">
+        <Tag className={`${className?.includes("text-white") && "text-white"}`}>
           {title}
-        </div>
+        </Tag>
         <div className="w-[30%] h-1.5 rounded-sm bg-[#F8C14C]" />
       </div>
     </div>
