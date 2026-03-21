2b28b3e5efb060e5dd4cb46197a4125bc6787c6d
studenthousingmumbai <117896643+studenthousingmumbai@users.noreply.github.com>
2026-03-21 16:54:12 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,6 +6,7 @@ import { Popover, Transition, Dialog, Menu } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import useAuth from "../hooks/useAuth";
 import withAuth from "../hooks/withAuth";
+import Script from "next/script";
 
 
 const hostels = [
@@ -77,6 +78,18 @@ export default function Example() {
   }, []);
 
   return (
+
+      <>
+    <Script id="microsoft-clarity" strategy="afterInteractive">
+      {`
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "vz5p8bo23l");
+      `}
+    </Script>
+    
     <Popover
       className={`bg-white transition-shadow duration-500 ease-in-out shadow-sm ${
         scrolled ? "shadow-md" : ""
