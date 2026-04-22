21a350c3843142511446a80075e9559713043fe0
studenthousingmumbai <117896643+studenthousingmumbai@users.noreply.github.com>
2026-04-22 16:51:02 +0530

    Update signin.jsx

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -53,9 +53,9 @@ export default function Signin() {
                   alt="Your Company"
                 />
               </Link>
-              <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
+              <h1 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
                 Sign in to your account
-              </h2>
+              </h1>
               <p className="mt-2 text-sm text-gray-600">
                 Or{" "}
                 <Link href="/signup" legacyBehavior>
