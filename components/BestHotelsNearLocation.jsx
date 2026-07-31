5062895c8ccddd27cfb8d23a8899ecae97f34250
studenthousingmumbai <studenthousing.co.in@gmail.com>
2026-07-31 15:00:00 +0530

    console log added

--- components/BestHotelsNearLocation.jsx
+++ components/BestHotelsNearLocation.jsx
@@ -5,6 +5,12 @@ import SectionTitle from "./SectionTitle";
 
 export default function BestHotelsNearLocation({ data }) {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+
+  console.log("BestHotelsNearLocation data:", data);
+console.log("Gender:", data?.gender);
+
+
   return (
     <div className="relative py-12 responsiveCenterPadding">
       <div className="absolute inset-0 bg-[url(/bg-pattern-2.png)] bg-no-repeat z-0" />
